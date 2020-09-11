6d222e5b972a783182e76f7dca0fc28b17ffe666
Samujjwaal Dey <sdey9@uic.edu>
2020-09-11 16:01:26 -0500

    Test logback logging

--- build.sbt
+++ build.sbt
@@ -4,5 +4,9 @@ version := "0.1"
 
 scalaVersion := "2.13.3"
 
+
+// https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.3.0-alpha5"
+
 // https://mvnrepository.com/artifact/com.typesafe/config
 libraryDependencies += "com.typesafe" % "config" % "1.4.0"
--- src/main/java/TestDependency.java
+++ src/main/java/TestDependency.java
@@ -1,8 +1,16 @@
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class TestConfig {
+public class TestDependency {
+
+    //Define a static logger variable so that it references the Logger instance
+    private static final Logger logger = LoggerFactory.getLogger(TestDependency.class);
+
     public static void main(String[] args) {
+
+        logger.info("Parsing input from default config file ");
 
         Config paramsConfig = ConfigFactory.load("default.conf");
         Config[] iterations = paramsConfig.getConfigList("iterations").toArray(new Config[0]);
@@ -13,5 +21,6 @@ public class TestConfig {
         System.out.println(testConfig.getString("conf.combined"));
         System.out.println(testConfig.getString("featureFlags.featureA"));
 
+        logger.info("End of execution");
     }
 }
