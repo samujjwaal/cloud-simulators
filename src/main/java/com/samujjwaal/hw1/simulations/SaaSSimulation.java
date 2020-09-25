2d8bc72686b77352b3b417e5e9321102d3c87207
Samujjwaal Dey <sdey9@uic.edu>
2020-09-25 01:18:19 -0500

    Updated SaaS simulation class

--- src/main/java/com/samujjwaal/hw1/simulations/SaaSSimulation.java
+++ src/main/java/com/samujjwaal/hw1/simulations/SaaSSimulation.java
@@ -5,8 +5,6 @@ import org.cloudbus.cloudsim.brokers.DatacenterBroker;
 import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple;
 import org.cloudbus.cloudsim.cloudlets.Cloudlet;
 import org.cloudbus.cloudsim.core.CloudSim;
-import org.cloudbus.cloudsim.datacenters.Datacenter;
-import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelFull;
 import org.cloudbus.cloudsim.vms.Vm;
 import org.cloudsimplus.builders.tables.CloudletsTableBuilder;
 import org.slf4j.Logger;
@@ -20,6 +18,7 @@ public class SaaSSimulation {
         //Define a static logger variable so that it references the Logger instance
         Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
         logger.info("Initiating SaaS Simulation\n");
+
         String simulationModel = "SaaSSim";
         int index = 1;
 
@@ -34,12 +33,13 @@ public class SaaSSimulation {
 
         logger.info("Parsed datacenter specifications from config\n");
 
-        // create instance of util class
+        // create instance of util class for datacenter operations
         DataCenterUtils dcUtil = new DataCenterUtils(hostSpec,dcSpec,vmSpec,cloudletSpec);
 
-        Datacenter dc = dcUtil.createDatacenter(simulation);
+        // create a new datacenter for simulation
+        dcUtil.createDatacenter(simulation);
 
-        // Creates a broker that is a software acting on behalf a cloud customer to manage his/her VMs and Cloudlets
+        // Creates a broker that is a software acting on behalf a cloud customer to manage VMs and Cloudlets
         DatacenterBroker broker0 = new DatacenterBrokerSimple(simulation);
 
         Vm vm = dcUtil.createVm();
@@ -47,7 +47,7 @@ public class SaaSSimulation {
         // submit vm list to broker
         broker0.submitVm(vm);
 
-        Cloudlet cloudlet = dcUtil.createCloudlet(new UtilizationModelFull());
+        Cloudlet cloudlet = dcUtil.createCloudlet();
 
         // submit cloudlet list to broker
         broker0.submitCloudlet(cloudlet);
@@ -62,6 +62,7 @@ public class SaaSSimulation {
         System.out.println("Total cost of simulation = "+dcUtil.executionCost(finishedCloudlets));
         System.out.println("\n");
 
+        logger.info("End of execution of SaaS Simulation\n\n");
     }
 
 
