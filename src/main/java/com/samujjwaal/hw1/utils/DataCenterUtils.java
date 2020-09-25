d0ab6d928307ed03aa6ae628eba6a63d4737b181
Samujjwaal Dey <sdey9@uic.edu>
2020-09-25 03:01:37 -0500

    Updated DataCenterUtils - Added createVmList() & createCloudletList() - Overloaded definition of some methods - Converted imperative for loops to functional

--- src/main/java/com/samujjwaal/hw1/utils/DataCenterUtils.java
+++ src/main/java/com/samujjwaal/hw1/utils/DataCenterUtils.java
@@ -1,5 +1,7 @@
 package com.samujjwaal.hw1.utils;
 
+import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicy;
+import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicySimple;
 import org.cloudbus.cloudsim.cloudlets.Cloudlet;
 import org.cloudbus.cloudsim.cloudlets.CloudletSimple;
 import org.cloudbus.cloudsim.core.CloudSim;
@@ -10,6 +12,7 @@ import org.cloudbus.cloudsim.hosts.HostSimple;
 import org.cloudbus.cloudsim.resources.Pe;
 import org.cloudbus.cloudsim.resources.PeSimple;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
+import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelFull;
 import org.cloudbus.cloudsim.vms.Vm;
 import org.cloudbus.cloudsim.vms.VmSimple;
 import org.slf4j.Logger;
@@ -17,6 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class DataCenterUtils {
     //Define a static logger variable so that it references the Logger instance
@@ -32,35 +36,52 @@ public class DataCenterUtils {
         this.configDatacenter = configDatacenter;
         this.configVM = configVM;
         this.configCloudlet = configCloudlet;
+
+        logger.info("Datacenter specifications loaded from the configuration file are:");
+        logger.info("Number of Hosts: " + configDatacenter.numberOfHosts);
+        logger.info("Number of Vms: " + configDatacenter.numberOfVms);
+        logger.info("Number of Cloudlets: " + configDatacenter.numberOfCloudlets + "\n");
     }
 
-    public Host createHost() {
-        final List<Pe> peList = new ArrayList<>(configHost.numberOfPE);
+    public List<Pe> createPeList() {
+        final List<Pe> list = new ArrayList<>();
+        IntStream.range(0, configHost.numberOfPE)
+                .forEach(i -> {
+                    list.add(new PeSimple(configHost.mips));
+                    logger.info("PE " + i + " added to host");
+                });
+        return list;
+    }
+
+    public Host createHost(boolean activateHost) {
         //List of Host's CPUs (Processing Elements, PEs)
-        for (int i = 0; i < configHost.numberOfPE; i++) {
-            //Uses a PeProvisionerSimple by default to provision PEs for VMs
-            peList.add(new PeSimple(configHost.mips));
-            logger.info("PE "+i+" added to host");
-        }
+        final List<Pe> peList = createPeList();
         /*
         Uses ResourceProvisionerSimple by default for RAM and BW provisioning
         and VmSchedulerSpaceShared for VM scheduling.
         */
-        return new HostSimple(configHost.ram, configHost.bw, configHost.storage, peList);
+        return new HostSimple(configHost.ram, configHost.bw, configHost.storage, peList, activateHost);
     }
 
-    public Datacenter createDatacenter(CloudSim simulation) {
+    public List<Host> createHostList(boolean activateHosts) {
+        final List<Host> list = new ArrayList<>();
+        logger.info("Adding hosts " + configDatacenter.numberOfHosts + " to the datacenter");
+        IntStream.range(0, configDatacenter.numberOfHosts)
+                .forEach(i -> {
+                    logger.info("Creating host " + i + " and adding PEs ");
+                    list.add(createHost(activateHosts));
+                    logger.info("Host " + i + " added to datacenter");
+                });
+        System.out.println();
+        return list;
+    }
+
+    public Datacenter createDatacenter(CloudSim simulation, VmAllocationPolicy vmPolicy, boolean activateHosts) {
         //List of Datacenter's Hosts
-        final List<Host> hostList = new ArrayList<>(configDatacenter.numberOfHosts);
-        for(int i = 0; i < configDatacenter.numberOfHosts; i++) {
-            logger.info("Creating host "+i + " and adding PEs ");
-            Host host = createHost();
-            hostList.add(host);
-            logger.info("Host "+i+" added to datacenter");
-        }
+        final List<Host> hostList = createHostList(activateHosts);
 
         //Uses a VmAllocationPolicySimple by default to allocate VMs
-        Datacenter dc = new DatacenterSimple(simulation, hostList);
+        Datacenter dc = new DatacenterSimple(simulation, hostList, vmPolicy);
         // set characteristics of the datacenter
         dc.getCharacteristics()
                 .setArchitecture(configDatacenter.arch)
@@ -73,25 +94,62 @@ public class DataCenterUtils {
         return dc;
     }
 
-    public Vm createVm(){
+    public Datacenter createDatacenter(CloudSim simulation, VmAllocationPolicy vmPolicy) {
+        return createDatacenter(simulation, vmPolicy, true);
+    }
+
+    public Datacenter createDatacenter(CloudSim simulation, boolean activateHosts) {
+        return createDatacenter(simulation, new VmAllocationPolicySimple(), activateHosts);
+    }
+
+    public Datacenter createDatacenter(CloudSim simulation) {
+        return createDatacenter(simulation, true);
+    }
+
+    public Vm createVm() {
         // return new vm instance
-        return new VmSimple(configVM.mips,configVM.numberOfPE)
+        return new VmSimple(configVM.mips, configVM.numberOfPE)
                 .setRam(configVM.ram)
                 .setBw(configVM.bw)
                 .setSize(configVM.size);
     }
 
-    public Cloudlet createCloudlet(UtilizationModel utilizationModel){
+    public List<Vm> createVmList() {
+        final List<Vm> list = new ArrayList<>();
+        logger.info("Provisioning " + configDatacenter.numberOfVms + " Vms for allocation to datacenter hosts\n");
+        IntStream.range(0, configDatacenter.numberOfVms)
+                .forEach(i -> list.add(createVm()));
+        return list;
+    }
+
+    public Cloudlet createCloudlet(UtilizationModel utilizationModel) {
         // return new cloudlet instance
-        return new CloudletSimple(configCloudlet.length,configCloudlet.numberOfPE, utilizationModel)
+        return new CloudletSimple(configCloudlet.length, configCloudlet.numberOfPE, utilizationModel)
                 .setSizes(configCloudlet.size);
     }
 
-    public float executionCost(List<Cloudlet> cloudlets){
+    public Cloudlet createCloudlet() {
+        return createCloudlet(new UtilizationModelFull());
+    }
+
+    public List<Cloudlet> createCloudletList(UtilizationModel utilizationModel) {
+        final List<Cloudlet> list = new ArrayList<>();
+        logger.info("Received " + configDatacenter.numberOfCloudlets + " cloudlets for execution in the datacenter\n");
+        IntStream.range(0, configDatacenter.numberOfVms)
+                .forEach(i -> list.add(createCloudlet(utilizationModel)));
+        return list;
+    }
+
+    public List<Cloudlet> createCloudletList() {
+        return createCloudletList(new UtilizationModelFull());
+    }
+
+
+    public float executionCost(List<Cloudlet> cloudlets) {
         float cost = 0.0f;
-        for (Cloudlet cl : cloudlets){
+        for (Cloudlet cl : cloudlets) {
             cost += cl.getTotalCost();
-            logger.info("Cost to execute cloudlet "+cl.getId()+" = "+(float)cl.getTotalCost());
+            logger.info("Cost to execute cloudlet " + cl.getId() + " = " + (float) cl.getTotalCost());
         }
         return cost;
     }
